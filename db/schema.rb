93909ffbbe33817fb513b253bbd1321edb53fa8e
vagrant <vagrant@localhost.localdomain>
2020-11-04 23:27:11 +0000

    [Fix]layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_144522) do
+ActiveRecord::Schema.define(version: 2020_11_03_151346) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 2020_11_03_144522) do
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visitor_id"
+    t.integer "visited_id"
+    t.integer "room_id"
+    t.integer "message_id"
+    t.string "action"
+    t.boolean "checked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rooms", force: :cascade do |t|
